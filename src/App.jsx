147003304228befaceb8b9add58e9d5bc6ba561b
JayPachchigar0904 <pachchigarjay94@gmail.com>
2024-10-02 17:37:01 +0530

    created header

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from './Pages/SignIn'
 import SignUp from './Pages/SignUp'
 import About from './Pages/About'
 import Profile from './Pages/Profile'
+import Header from './Components/Header'
 
 function App() {
   return (
 <BrowserRouter>
+<Header />
   <Routes>
 
     <Route path="/" element={<Home/>} />
@@ -16,7 +18,7 @@ function App() {
     <Route path="/sign-up" element={<SignUp />} />
     <Route path="/about" element={<About />} />
     <Route path="/profile" element={<Profile />} />
-    
+
   </Routes>
 </BrowserRouter>
   ) 
