a1af311757832c23ddebb9c14874204b92b42425
JayPachchigar0904 <pachchigarjay94@gmail.com>
2025-01-21 00:00:57 +0530

    completed image upload functionality

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -16,13 +16,13 @@ function Header() {
   </Link>
   <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
     <input type = "text" placeholder = "Search..." className='bg-transparent focus:outline-none sm:w-64'/>
-    <FaSearch className='text-slate-600'/>
+    <FaSearch className='text-slate-600'/> 
   </form>
   <ul className = "flex gap-4 items-center">
   <Link to = "/"><li className='hidden sm:inline text-slate-700 hover:underline'>Home</li></Link>
   <Link  to = "/about"><li className='hidden sm:inline text-slate-700 hover:underline'>About</li></Link>
   <Link  to = "/profile">
-   {currentUser ? (<img className = 'rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt = "profile"/>) : (<li className='text-slate-700 hover:underline'>Sign In</li>)}
+   {currentUser ? (<img className = 'rounded-full h-7 w-7 object-cover' src = {currentUser.avatar} alt = "profile"/>) : (<li className='text-slate-700 hover:underline'>Sign In</li>)}
    </Link>
   </ul>
   </div>
--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRef } from 'react'
 import { useSelector } from 'react-redux'
 
 function Profile() {
+  const fileRef = useRef(null)
+  const [formData,setFormData] = useState({})
   const {currentUser} = useSelector((state) => state.user)
   const handleChange = () => {
 
   }
-  return (
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    if(!file) return;
+    const data = new FormData();
+    data.append("file",file)
+    data.append("upload_preset", "My-Estate")
+    data.append("cloud_name","dati1ti2a")
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/dati1ti2a/image/upload",{
+      method : 'POST',
+      body : data,
+    })
+
+    const uploaded_image = await res.json()
+    //console.log(uploaded_image)
+    setFormData({...formData,avatar : uploaded_image.url});
+  }
+  //console.log(currentUser.avatar)
+  return (  
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form className='flex flex-col gap-4 mx-auto'>
-        <img src={currentUser.avatar} className='h-24 w-24 rounded-full object-cover self-center mt-2 cursor-pointer' alt = "profile picture"/>
+      <input type = "file" ref = {fileRef} hidden accept = "image/*" onChange = {handleFileChange}/>
+        <img src = {formData.avatar ? formData.avatar : currentUser.avatar} className='h-24 w-24 rounded-full object-cover self-center mt-2 cursor-pointer' alt = "Profile Picture  " onClick = {() => {fileRef.current.click()}}/>
         <input type = 'text' placeholder='username' className='border p-3 rounded-lg gap-4' id = 'username' onChange = {handleChange}/>
         <input type = 'email' placeholder='email' className='border p-3 rounded-lg gap-4' id = 'email' onChange = {handleChange}/>
         <input type = 'password' placeholder='password' className='border p-3 rounded-lg gap-4' id = 'password' onChange = {handleChange}/>
