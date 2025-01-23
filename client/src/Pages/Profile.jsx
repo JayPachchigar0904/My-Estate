6ace1b29440bdb6a6169fcab4c50ea97a27a508c
JayPachchigar0904 <pachchigarjay94@gmail.com>
2025-01-23 23:12:14 +0530

    completed update user functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react'
 import { useRef } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUserFailure,updateUserSuccess,updateUserStart } from '../Redux/users/userSlice'
 
 function Profile() {
   const fileRef = useRef(null)
   const [formData,setFormData] = useState({})
-  const {currentUser} = useSelector((state) => state.user)
-  const handleChange = () => {
-
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+  const dispatch = useDispatch()
+  const {currentUser, loading, error} = useSelector((state) => state.user)
+  const handleChange = (e) => {
+    setFormData({...formData,[e.target.id] : e.target.value})
   }
   const handleFileChange = async (e) => {
     const file = e.target.files[0];
@@ -26,23 +29,49 @@ function Profile() {
     //console.log(uploaded_image)
     setFormData({...formData,avatar : uploaded_image.url});
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try{
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`,{
+        method : 'POST',
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success === false){
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } 
+    catch(error){
+     dispatch(updateUserFailure(error.message));
+    }
+  }
   //console.log(currentUser.avatar)
   return (  
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4 mx-auto'>
+      <form className='flex flex-col gap-4 mx-auto' onSubmit = {handleSubmit}>
       <input type = "file" ref = {fileRef} hidden accept = "image/*" onChange = {handleFileChange}/>
         <img src = {formData.avatar ? formData.avatar : currentUser.avatar} className='h-24 w-24 rounded-full object-cover self-center mt-2 cursor-pointer' alt = "Profile Picture  " onClick = {() => {fileRef.current.click()}}/>
-        <input type = 'text' placeholder='username' className='border p-3 rounded-lg gap-4' id = 'username' onChange = {handleChange}/>
-        <input type = 'email' placeholder='email' className='border p-3 rounded-lg gap-4' id = 'email' onChange = {handleChange}/>
-        <input type = 'password' placeholder='password' className='border p-3 rounded-lg gap-4' id = 'password' onChange = {handleChange}/>
-        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+        <input type = 'text' placeholder='username' defaultValue={currentUser.username} className='border p-3 rounded-lg gap-4' id = 'username' onChange = {handleChange}/>
+        <input type = 'email' placeholder='email' defaultValue={currentUser.email} className='border p-3 rounded-lg gap-4' id = 'email' onChange = {handleChange}/>
+        <input type = 'password' placeholder='password'  className='border p-3 rounded-lg gap-4' id = 'password' onChange = {handleChange}/>
+        <button disabled = {loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...':'Update'}</button>
       </form>
 
       <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign Out</span>
       </div>
+      <p className='text-red-700 mt-5'>{error ? error : ''}</p>
+      <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully!' : ''}</p>
     </div>
   )
 }
